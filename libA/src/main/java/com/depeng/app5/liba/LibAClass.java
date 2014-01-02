1e7b108c89e7b2d1f187782d8c09f14c414da4f3
Cong Depeng <congdepeng@gmail.com>
2014-01-02 23:31:20 +0800

    finished

---
+++ libA/src/main/java/com/depeng/app5/liba/LibAClass.java
@@ -0,0 +1,10 @@
+package com.depeng.app5.liba;
+
+/**
+ * Created by depeng on 2/1/14.
+ */
+public class LibAClass {
+    public static void print(){
+        System.out.println("lib a");
+    }
+}
---
+++ libB/src/main/java/com/depeng/app5/libb/LibBClass.java
@@ -0,0 +1,10 @@
+package com.depeng.app5.libb;
+
+/**
+ * Created by depeng on 2/1/14.
+ */
+public class LibBClass {
+    public static void print(){
+        System.out.println("lib b");
+    }
+}
